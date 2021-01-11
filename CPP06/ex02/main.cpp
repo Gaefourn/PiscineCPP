12bb08bb1dfa87bdd2398787acf605ff34127224
Gaetan FOURNIER <gaefourn@e3r10p14.42.fr>
2021-01-11 17:54:07 +0100

    ajout ex03

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,54 @@
+#include "Classes.hpp"
+#include <iostream>
+
+void identify_from_pointer(Base * p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+
+void identify_from_reference(Base & p)
+{
+	try
+	{
+		A & a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+	}
+	try
+	{
+		B & b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+	}
+	try
+	{
+		C & c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+	}
+}
+
+int main()
+{
+	Base *a = new A();
+	Base *b = new B();
+	Base *c = new C();
+	Base &ref = *c;
+	
+	identify_from_pointer(a);
+	identify_from_reference(ref);
+
+	return 0;
+}
