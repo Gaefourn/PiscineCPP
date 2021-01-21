2a50c1e4e16d58d6566d2a1e49167e921ccfebb9
Gaetan FOURNIER <gaefourn@e3r3p10.42.fr>
2021-01-21 15:07:46 +0100

    c mieux comme ca

--- CPP03/ex04/SuperTrap.cpp
+++ CPP03/ex04/SuperTrap.cpp
@@ -2,6 +2,9 @@
 
 SuperTrap::SuperTrap(std::string name) : ClapTrap(name), NinjaTrap(name), FragTrap(name)
 {
+	_Energy_points = 120;
+	_Max_energy_points = 120;
+	_Melee_attack_damage = 60;
 	std::cout << _Name << " : Bonjour, je suis un super trap !" << std::endl;
 }
 
